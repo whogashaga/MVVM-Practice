113f81acd07843183c0e69a894032000fe20009d
Kerry <kerrychen0815@gmail.com>
2019-07-11 14:05:14 +0800

    save point

--- app/src/main/java/com/example/mvvmassignment/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mvvmassignment/ui/main/MainFragment.kt
@@ -3,7 +3,6 @@ package com.example.mvvmassignment.ui.main
 import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,14 +12,8 @@ import android.widget.ProgressBar
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.mvvmassignment.GetZooInfoCallback
 import com.example.mvvmassignment.R
-import com.example.mvvmassignment.retrofit.client.RetrofitClient
-import com.example.mvvmassignment.retrofit.service.ApiService
 import com.example.mvvmassignment.utils.InjectUtils
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 
 class MainFragment : Fragment() {
 
@@ -37,13 +30,11 @@ class MainFragment : Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        viewModel = ViewModelProviders.of(this, factory).get(MainViewModel::class.java)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         // TODO: Use the ViewModel
-        initializeViewModel()
     }
 
     override fun onCreateView(
@@ -51,7 +42,7 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val root = inflater.inflate(R.layout.main_fragment, container, false)
+        val root = inflater.inflate(R.layout.fragment_main, container, false)
         progressBar = root.findViewById(R.id.progressBar)
         btnSetData = root.findViewById(R.id.button_setData)
         recyclerView = root.findViewById(R.id.recycler_main_fragment)
@@ -59,6 +50,10 @@ class MainFragment : Fragment() {
             layoutManager = LinearLayoutManager(activity)
             adapter = mAdapter
         }
+
+        viewModel = ViewModelProviders.of(this, factory).get(MainViewModel::class.java)
+
+        initializeViewModel()
         return root
     }
 
